68e523297fa564764cf0e50eae421fc041416e07
santicantero <pepesperez4@gmail.com>
2026-03-11 12:07:41 +0100

    feat: guardar usuario google en callback

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
