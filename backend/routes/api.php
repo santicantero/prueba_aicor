48f9b3593eabd03f037df3df08e80fcf78897f0e
santicantero <pepesperez4@gmail.com>
2026-03-12 08:00:30 +0100

    chore: ajustar rutas api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/auth/google/redirect', [AuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
